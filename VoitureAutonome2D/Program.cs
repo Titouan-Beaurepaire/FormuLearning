979e0523b2f1759df34d539358536748b7b54541
Titouan Beaurepaire <titouan.beaurepaire@epitech.eu>
2025-06-18 11:24:40 +0200

    [ADD]{Camera to follow the car}

--- VoitureAutonome2D/Program.cs
+++ VoitureAutonome2D/Program.cs
@@ -10,19 +10,24 @@ class Program
         window.Closed += (_, __) => window.Close();
 
         var carTexture = new Texture("car.png");
-        var voiture = new Sprite(carTexture) {
-            Position = new Vector2f(30, 30)
+        var voiture = new Sprite(carTexture)
+        {
+            Position = new Vector2f(100, 400),
+            Origin = new Vector2f(carTexture.Size.X / 2, carTexture.Size.Y / 2)
         };
 
+        var view = new View(new FloatRect(0, 0, 800, 600));
         window.SetFramerateLimit(60);
 
         while (window.IsOpen)
         {
             window.DispatchEvents();
-
             voiture.Position += new Vector2f(1f, 0);
+            view.Center = voiture.Position;
+            window.SetView(view);
 
             window.Clear(Color.Black);
+
             window.Draw(voiture);
             window.Display();
         }
--- VoitureAutonome2D/obj/Debug/net8.0/VoitureAutonome2D.AssemblyInfo.cs
+++ VoitureAutonome2D/obj/Debug/net8.0/VoitureAutonome2D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoitureAutonome2D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddfda7e6f28b0eef6bb6acdb5eed83e53d5f3e56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d4f574f86455002478d4dc43fe52a60f92c742")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoitureAutonome2D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoitureAutonome2D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
