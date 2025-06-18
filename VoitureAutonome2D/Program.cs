d2d4f574f86455002478d4dc43fe52a60f92c742
Titouan Beaurepaire <titouan.beaurepaire@epitech.eu>
2025-06-18 11:06:51 +0200

    [ADD]{Project base and car texture added}

---
+++ VoitureAutonome2D/Program.cs
@@ -0,0 +1,30 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+class Program
+{
+    static void Main()
+    {
+        var window = new RenderWindow(new VideoMode(800, 600), "Voiture autonome 2D");
+        window.Closed += (_, __) => window.Close();
+
+        var carTexture = new Texture("car.png");
+        var voiture = new Sprite(carTexture) {
+            Position = new Vector2f(30, 30)
+        };
+
+        window.SetFramerateLimit(60);
+
+        while (window.IsOpen)
+        {
+            window.DispatchEvents();
+
+            voiture.Position += new Vector2f(1f, 0);
+
+            window.Clear(Color.Black);
+            window.Draw(voiture);
+            window.Display();
+        }
+    }
+}
