013183ab77966ae4a4393aa3bb23929e34bf5206
Titouan Beaurepaire <titouan.beaurepaire@epitech.eu>
2025-06-18 11:36:59 +0200

    [ADD]{Track}

--- VoitureAutonome2D/Program.cs
+++ VoitureAutonome2D/Program.cs
@@ -9,25 +9,35 @@ class Program
         var window = new RenderWindow(new VideoMode(800, 600), "Voiture autonome 2D");
         window.Closed += (_, __) => window.Close();
 
+        Color backgroundColor = new Color(128, 128, 128);
+
+        var trackTexture = new Texture("track.png");
+        var trackSprite = new Sprite(trackTexture)
+        {
+            Position = new Vector2f(0, 0),
+            Scale = new Vector2f(2f, 2f)  // Grossit le circuit x2
+        };
+
         var carTexture = new Texture("car.png");
         var voiture = new Sprite(carTexture)
         {
-            Position = new Vector2f(100, 400),
-            Origin = new Vector2f(carTexture.Size.X / 2, carTexture.Size.Y / 2)
+            Position = new Vector2f(150, 300)  // Position de départ sur le circuit agrandi
         };
 
-        var view = new View(new FloatRect(0, 0, 800, 600));
         window.SetFramerateLimit(60);
+
+        var view = new View(new FloatRect(0, 0, 800, 600));
 
         while (window.IsOpen)
         {
             window.DispatchEvents();
-            voiture.Position += new Vector2f(1f, 0);
-            view.Center = voiture.Position;
+
+
+            view.Center = voiture.Position + new Vector2f(voiture.GetGlobalBounds().Width / 2, voiture.GetGlobalBounds().Height / 2);
             window.SetView(view);
 
-            window.Clear(Color.Black);
-
+            window.Clear(backgroundColor);
+            window.Draw(trackSprite);
             window.Draw(voiture);
             window.Display();
         }
--- VoitureAutonome2D/obj/Debug/net8.0/VoitureAutonome2D.AssemblyInfo.cs
+++ VoitureAutonome2D/obj/Debug/net8.0/VoitureAutonome2D.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VoitureAutonome2D")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d4f574f86455002478d4dc43fe52a60f92c742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+979e0523b2f1759df34d539358536748b7b54541")]
 [assembly: System.Reflection.AssemblyProductAttribute("VoitureAutonome2D")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VoitureAutonome2D")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
